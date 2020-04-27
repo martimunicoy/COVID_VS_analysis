b5e02a7fc7cdbe9fa4fb28f9f4db9a04591359e5
Marti Municoy <martimunicoy@gmail.com>
2020-04-27 17:37:05 +0200

    Updating subpocket analysis tests

--- tests/test_SubpocketAnalysis.sh
+++ tests/test_SubpocketAnalysis.sh
@@ -1,1 +1,1 @@
-python ../SubpocketAnalysis.py . --subpocket B:141 B:142 B:165 B:166 --subpocket B:21 B:41 B:46 B:49
+python ../SubpocketAnalysis.py sim1 sim2 --subpocket A:141 A:142 A:165 A:166 --subpocket A:21 A:41 A:46 A:49 --subpocket A:187 A:188 A:189 --subpocket_radii 4 4 6 --include_rejected_steps -r mod_report
--- tests/test_analyze_subpocket_intersections.sh
+++ tests/test_analyze_subpocket_intersections.sh
@@ -1,1 +1,1 @@
-python ../Scripts/analyze_subpocket_intersections.py subpockets.csv . -o intersections.csv -r mod_report
+python ../Scripts/analyze_subpocket_intersections.py sim1 sim2
