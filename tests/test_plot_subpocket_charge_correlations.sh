df4a07890ed6ca44333a9b9ad0080ead612d5836
Marti Municoy <martimunicoy@gmail.com>
2020-05-08 15:17:06 +0200

    Allowing positive and negative charge analysis

---
+++ tests/test_plot_subpocket_charge_correlations.sh
@@ -0,0 +1,5 @@
+python ../Scripts/plot_subpocket_charge_correlations.py my_custom_path/sim? --ic50 ic50.csv --charge_type net -o subpocket_net_charge_correlation_test.png
+
+python ../Scripts/plot_subpocket_charge_correlations.py my_custom_path/sim? --ic50 ic50.csv --charge_type positive -o subpocket_positive_charge_correlation_test.png
+
+python ../Scripts/plot_subpocket_charge_correlations.py my_custom_path/sim? --ic50 ic50.csv --charge_type negative -o subpocket_negative_charge_correlation_test.png
