42ce0b638225809a0e04716273442a39132a0b3b
Marti Municoy <martimunicoy@gmail.com>
2020-05-14 11:41:12 +0200

    Force and user prompt functionalities are added

--- Scripts/smart_rename.py
+++ Scripts/smart_rename.py
@@ -4,7 +4,8 @@
 # Standard imports
 import argparse as ap
 from pathlib import Path
-
+import sys
+from shutil import move
 
 # Script information
 __author__ = "Marti Municoy"
@@ -32,18 +33,57 @@ def parse_args():
     parser.add_argument('--debug',
                         dest='debug',
                         action='store_true',
-                        help='Debug the behavior of the script without doing' +
-                        'any real modification')
+                        help='Debug the behavior of the script without doing'
+                        + 'any real modification')
+
+    parser.add_argument('--force',
+                        dest='force',
+                        action='store_true',
+                        help='Force action without asking first')
 
     parser.set_defaults(debug=False)
+    parser.set_defaults(force=False)
 
     args = parser.parse_args()
 
-    return args.folders, args.delimiter, args.field, args.debug
+    return args.folders, args.delimiter, args.field, args.debug, args.force
+
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
 
 
 def main():
-    folders, delimiter, field, debug = parse_args()
+    folders, delimiter, field, debug, force = parse_args()
 
     fields = []
     for f in field.split(','):
@@ -67,15 +107,34 @@ def main():
                 new_name.append(name.split(delimiter)[f])
             new_name = '_'.join(new_name)
         except IndexError:
-            print(' - Skipping folder {} because chosen '.format(folder) +
-                  'fields were out of range')
+            print(' - Skipping folder {} because chosen '.format(folder)
+                  + 'fields were out of range')
             continue
 
-        if (debug):
-            print(' - Folder {} would be renamed to {}'.format(
-                folder, new_name))
+        new_path = folder.parent.joinpath(new_name)
+
+        if new_path.is_dir():
+            action = 'Move'
         else:
-            folder.rename(new_name)
+            action = 'Rename'
+
+        if (debug):
+            print(' - Folder {} would be {}d to {}'.format(
+                folder, action.lower(), new_name))
+        else:
+            if not force:
+                if not query_yes_no('{} folder {} to {}?'.format(
+                        action, folder, new_name)):
+                    return
+
+            print(' - {}ing {} to {}'.format(action[:-1], folder, new_name))
+            if action == 'Move':
+                for f in folder.iterdir():
+                    move(str(f), str(new_path))
+                folder.rmdir()
+
+            elif action == 'Rename':
+                folder.rename(new_name)
 
 
 if __name__ == "__main__":
